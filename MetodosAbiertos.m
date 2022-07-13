95334ed19a39db23342076e26e2e0c4830e784e9
Jose <david-restrepo_123@hotmail.es>
2022-07-13 13:43:56 -0500

    Subida Newton Raphson y Punto Fijo
    Se terminó el Metodo de Newton Rahpson funciona completo y se realizó el Metodo de Punto Fijo que tambien funciona completo

--- MetodosAbiertos.m
+++ MetodosAbiertos.m
@@ -2,27 +2,30 @@ function MetodosAbiertos()
     Men=0;
     while(Men!=4)
       try
-        Men=menu("Menu Metodos Abiertos","Metodo de Newton Raphson",
+        Men=menu("Menu de Metodos Abiertos","Metodo de Newton Raphson",
         "Punto fijo", "Metodo de la secante","Atras")
         switch(Men)
           case 1
             N_R()
+          case 2
+            PF()
         endswitch
 
       catch
-        waitfor(msgbox("Error en el menu de metodos Abiertos","Error"))
+        waitfor(msgbox("Error en el menu de Metodos Abiertos","Error"))
       end_try_catch
    endwhile
 
 endfunction
 
+#Codigo Newton Rahpson
 function N_R()
   try
     E=100;
     #xi=0;
     #func(xi)=0;
     #fdev(xi)=0;
-    xi = ValidarUndato();
+    xi = ValidarUndato("xi");
     func = PedirFormula();
     fdev = PedirDerivada();
 
@@ -31,15 +34,22 @@ function N_R()
 
       fcn = feval(func,xi);
       derv = feval(fdev,xi);
-      xi_s = xi - (fnc/derv);
 
       if(i ~= 0)
-        E = (abs((xi_s - xi)/xi_s))*100;
-      else
-        E = 100;
+        E = (abs((xi_s - ant)/xi_s))*100;
       endif
-    endwhile
+      xi_s = xi - (fcn/derv);
+      ant = xi;
 
+      xvec(i+1) = xi;
+      fvec(i+1) = fcn;
+      dvec(i+1) = derv;
+      Evec(i+1) = E;
+
+      xi = xi_s;
+      i++;
+    endwhile
+    Mostrar(xvec,fvec,dvec,Evec);
   catch
     waitfor(msgbox("Error en Newton Rahpson","Error"));
   end_try_catch
@@ -108,3 +118,153 @@ function dato=ValidarUndato(msg)
      end_try_catch
    endwhile
  endfunction
+
+#Funcion para mostrar Metodo Newton Raphson
+function Mostrar(xvec,fvec,dvec,Evec)
+Men1=0;
+  while(Men1!=4)
+    try
+      Men1=menu("Menu Mostrar",
+      "Tabla", "Valor de la raíz con error menor 0.001","Todo",  "Atras")
+      switch(Men1)
+        case 1
+           answer=[1,xvec(1),fvec(1),dvec(1),100000];
+
+           for i=2:length(xvec)
+            newLine = [i,xvec(i),fvec(i),dvec(i),Evec(i)];
+            before = answer;
+            answer = [before; newLine];
+           endfor
+           answer=num2str(answer)
+           newLine=[" i |"," X |"," F(xi) |","F_d(xi)"," Error "];
+           before = answer;
+           answer = [newLine; before];
+           waitfor(msgbox(num2str(answer),"Tabla"))
+        case 2
+           raiz=xvec(length(xvec));
+           waitfor(msgbox(sprintf("La raiz mas proxima es %d",raiz),"Raiz"))
+        case 3
+           answer=[1,xvec(1),fvec(1),dvec(1),100000];
+           for i=2:length(xvec)
+           newLine = [i,xvec(i),fvec(i),dvec(i),Evec(i)];
+           before = answer;
+           answer = [before; newLine];
+         endfor
+          answer=num2str(answer);
+           newLine=[" i |"," X |"," F(xi) |"," F_d(xi) |"," Error "];
+           before = answer;
+           answer = [newLine; before];
+           waitfor(msgbox(num2str(answer),"Tabla"))
+           raiz=xvec(length(xvec));
+           waitfor(msgbox(sprintf("La raiz mas proxima es %d",raiz),"Raiz"))
+          case 4
+        otherwise
+           waitfor(msgbox("Debe seleccionar una opcion correcta"))
+      endswitch
+    catch err
+      waitfor(msgbox(err.identifier, err.message));
+      waitfor(msgbox("Error en el menu principal","Error"));
+    end_try_catch
+  endwhile
+endfunction
+
+#Metodo Punto Fijo
+function PF()
+  try
+    E = 100;
+    xi =ValidarUndato("xi");
+    gx = PedirEcn();
+    i = 0;
+    while (E > (1*10^(-3)))
+
+      fcn = feval(gx,xi);
+
+      if(i ~= 0)
+        E = (abs((xi - ant)/xi))*100;
+      endif
+      ant = xi;
+
+      vecx(i+1) = fcn;
+      vecE(i+1) = E;
+
+      xi = fcn;
+      i++;
+    endwhile
+    MostrarPF(vecx,vecE);
+  catch
+    waitfor(msgbox("Error en Punto Fijo","Error"));
+  end_try_catch
+endfunction
+
+function funct=PedirEcn()
+   ban=1;
+   while ban!=0
+     try
+      funcInput=inputdlg ("Ingrese funcion despejada de la forma g(x)")
+      eval(['funct = @(x) (' funcInput{1,1} ');']);
+      x=[1 1 1 1 1 1];
+      funct(x);
+      ban=0;
+     catch err
+      ban=1;
+      if(err.identifier=="Octave:undefined-function")
+       waitfor(msgbox ("La funcion solo recibe una variable","Error"))
+     else
+        waitfor(msgbox("Error al ingresar la funcion","Error"))
+      endif
+     end_try_catch
+   endwhile
+ endfunction
+#Funcion para Mostrar el Metodo Punto Fijo
+function MostrarPF(vecx,vecE)
+Men1=0;
+  while(Men1!=4)
+    try
+      Men1=menu("Menu Mostrar",
+      "Tabla", "Valor de la raíz con error menor 0.001","Todo",  "Atras")
+      switch(Men1)
+        case 1
+           answer=[1,vecx(1),100000];
+
+           for i=2:length(vecx)
+            newLine = [i,vecx(i),vecE(i)];
+            before = answer;
+            answer = [before; newLine];
+           endfor
+           answer=num2str(answer)
+           newLine=[" i |"," G(xi) |"," Error "];
+           before = answer;
+           answer = [newLine; before];
+           waitfor(msgbox(num2str(answer),"Tabla"))
+        case 2
+           raiz=vecx(length(vecx));
+           waitfor(msgbox(sprintf("La raiz mas proxima es %d",raiz),"Raiz"))
+        case 3
+           answer=[1,vecx(1),100000];
+           for i=2:length(vecx)
+           newLine = [i,vecx(i),vecE(i)];
+           before = answer;
+           answer = [before; newLine];
+         endfor
+          answer=num2str(answer);
+           newLine=[" i |"," G(xi) |"," Error "];
+           before = answer;
+           answer = [newLine; before];
+           waitfor(msgbox(num2str(answer),"Tabla"))
+           raiz=vecx(length(vecx));
+           waitfor(msgbox(sprintf("La raiz mas proxima es %d",raiz),"Raiz"))
+          case 4
+        otherwise
+           waitfor(msgbox("Debe seleccionar una opcion correcta"))
+      endswitch
+    catch err
+      waitfor(msgbox(err.identifier, err.message));
+      waitfor(msgbox("Error en el menu principal","Error"));
+    end_try_catch
+  endwhile
+endfunction
+
+#Metodo de la Secante
+
+
+
